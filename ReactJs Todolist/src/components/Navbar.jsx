b4116231cf325ddc1d95624fb5ad9a6926fd9137
Awais Khalil <122378039+AwaisKhalil19@users.noreply.github.com>
2026-04-08 21:47:52 +0500

    Add in navbar
    Todo-list Is Updated

--- ReactJs Todolist/src/components/Navbar.jsx
+++ ReactJs Todolist/src/components/Navbar.jsx
@@ -6,10 +6,6 @@ function Navbar() {
         <div className="logo">
             <span className='font-bold text-2xl mx-7'>iTask</span>
         </div>
-        <ul className='flex gap-8 mx-8'>
-            <li className='cursor-pointer text-lg transiton-all  hover:font-bold'>Home</li>
-            <li className='Cursor-pointer  text-lg transition-all  hover:font-bold'>Your-Task</li>
-        </ul>
     </nav>
   )
 }
